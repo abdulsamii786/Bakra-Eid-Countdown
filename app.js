543eb7bf0d64e06fd75e8c7fc4fc62bc6ff57259
abdulsamii786 <154473245+abdulsamii786@users.noreply.github.com>
2024-05-30 02:09:31 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let id = setInterval(eidCountdown, 1000);
+
 function eidCountdown() {
   let currentDate = new Date().getTime();
   let eidDate = new Date("Jun 17 2024").getTime();
@@ -12,14 +14,17 @@ function eidCountdown() {
 
   let getDays = difference / (1000 * 60 * 60 * 24);
 
-  let getDiv = document.getElementById("main");
+  let getMain = document.getElementById("main");
+  let getTitle = document.getElementById("title");
 
-  if (getSec === 0) {
-    getDiv.innerHTML = `<div class="box"><h1>Eid Mubarak</h1></div>`;
-    getDiv.style.fontSize = "40px";
-    getDiv.style.textAlign = "center";
+  if (difference == 0) {
+    clearInterval(id);
+    getMain.innerHTML = `<div class="box"><h1>Eid Mubarak</h1></div>`;
+    getMain.style.fontSize = "40px";
+    getMain.style.textAlign = "center";
   } else {
-    getDiv.innerHTML = `
+    getTitle.innerHTML = `<h1>Eid Ul Adha Countdown</h1>`;
+    getMain.innerHTML = `
     <div class="box"><h2>Days</h2><h2>${Math.floor(getDays)}</h2></div>
     <div class="box"><h2>Hours</h2><h2>${Math.floor(getHours % 24)}</h2></div>
     <div class="box"><h2>Minutes</h2><h2>${Math.floor(getMin % 60)}</h2></div>
@@ -27,6 +32,3 @@ function eidCountdown() {
     `;
   }
 }
-
-setInterval(eidCountdown, 1000);
-eidCountdown();
